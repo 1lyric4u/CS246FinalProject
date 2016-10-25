f3c7b4072cbedeba53e383f42a15f5fc380631ec
Scott Rutherford <rutherford.scott@gmail.com>
2016-10-25 12:39:44 -0700

    Initial commit

---
+++ app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.scott.cs246finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * This class is the starting point for our class final project in CS 246 of Fall 2016.
+ * 
+ *
+ * @author Scott Rutherford, Shanna-Mae Slight, David Rosevear
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

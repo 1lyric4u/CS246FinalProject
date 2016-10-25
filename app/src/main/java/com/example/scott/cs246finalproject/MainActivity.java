fc8946c39df5c3d42b27259ca45d20855f798245
shanna90 <sli15001@byui.edu>
2016-10-25 18:38:40 -0400

    testing....

--- app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java
+++ app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
  * @author Scott Rutherford, Shanna-Mae Slight, David Rosevear
  */
 
-//This is a test comment- Shanna
+//This is a test comment too- Shanna
 //This is also a comment - Scott
 //This is my comment - David
 

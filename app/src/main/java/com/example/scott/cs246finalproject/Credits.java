d89ba74589b53a52c591d1e15a57509570f929a8
Scott Rutherford <rutherford.scott@gmail.com>
2016-11-01 14:05:42 -0700

    added empty Credits.java class

---
+++ app/src/main/java/com/example/scott/cs246finalproject/Credits.java
@@ -0,0 +1,35 @@
+package com.example.scott.cs246finalproject;
+
+/**
+ * Created by scott on 11/1/16.
+ */
+
+public class Credits {
+
+    private int count;
+
+    // resets first day of the month
+    public static final int RESET_DAY = 1;
+
+    public Credits(){
+        this.count = 0;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void reset(){
+        // to-do
+    }
+
+    public void decrementCredit(){
+        // to-do
+    }
+
+    public boolean isResetDay(){
+        // to-do
+        return false;
+    }
+
+}

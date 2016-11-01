a45e131ff54734d93775d4ac9382bac05bf67706
Scott Rutherford <rutherford.scott@gmail.com>
2016-11-01 15:24:33 -0700

    empty test class

--- app/src/main/java/com/example/scott/cs246finalproject/CalendarConnecgtor.java
+++ app/src/main/java/com/example/scott/cs246finalproject/CalendarConnecgtor.java
@@ -1,10 +1,22 @@
 package com.example.scott.cs246finalproject;
 
+
+import java.util.List;
+
 /**
  * Created by scott on 11/1/16.
  */
 
 public class CalendarConnecgtor {
+
+
+    public boolean isValidViwer(){
+        return false;
+    }
+
+    public List<Object> getCalendarEvents(){
+        return null;
+    }
 
 
 }
---
+++ app/src/test/java/com/example/scott/cs246finalproject/CreditsTest.java
@@ -0,0 +1,37 @@
+package com.example.scott.cs246finalproject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by scott on 11/1/16.
+ */
+public class CreditsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void reset() throws Exception {
+
+    }
+
+    @Test
+    public void addCredit() throws Exception {
+
+    }
+
+    @Test
+    public void useCredit() throws Exception {
+
+    }
+
+}

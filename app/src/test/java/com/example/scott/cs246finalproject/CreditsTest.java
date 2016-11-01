a5d4d6405cf8e84780cd5141f314b0a9ead21d1d
shanna90 <sli15001@byui.edu>
2016-11-01 19:50:47 -0400

    Merge branch 'master' of https://github.com/1lyric4u/CS246FinalProject
    # Conflicts:
    #	app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java

--- app/src/test/java/com/example/scott/cs246finalproject/CreditsTest.java
+++ app/src/test/java/com/example/scott/cs246finalproject/CreditsTest.java
@@ -4,7 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Calendar;
 
 /**
  * Created by scott on 11/1/16.
@@ -12,16 +14,11 @@ import java.util.Date;
 public class CreditsTest {
 
     private Credits credit = new Credits();
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
+    private Date inPast = new Date(1990,1,1);
+    private Date good = new Date(2017,1,1);
+    private Date tooFar = new Date(2017,1,23);
+    private Calendar cal = Calendar.getInstance();
+    private Date today = cal.getTime();
 
     @Test
     public void reset() throws Exception {
@@ -30,13 +27,35 @@ public class CreditsTest {
 
     @Test
     public void addCredit() throws Exception {
-        credit.addCredit(new Date());
-        assert( credit.getCount() > 0);
+        //check that you can't add a credit for a past lesson
+        credit.addCredit(inPast);
+        assert( credit.getCount() == 0);
+        // check that you cannot add a lesson for today (less than 24 hrs notice)
+        cal.add(Calendar.DATE, 1);
+        Date tomorrow = cal.getTime();
+        credit.addCredit(tomorrow);
+        assert(credit.getCount() == 0);
+        cal.add(Calendar.DATE, -1); //reset calendar
+        // check that you can add a credit for a valid date
+        credit.addCredit(good);
+        assert(credit.getCount() == 1);
     }
 
     @Test
     public void useCredit() throws Exception {
-
+        credit.addCredit(good);
+        int count = credit.getCount();
+        //check that Credit cannot be used for today
+        credit.useCredit(today);
+        assert (credit.getCount() == count);
+        //check that credit cannot be used more than three weeks ahead of date
+        credit.useCredit(tooFar);
+        assert (credit.getCount() == count);
+        //check that credit can be used for valid date
+        cal.add(Calendar.DATE, 1);
+        Date tomorrow = cal.getTime();
+        credit.useCredit(tomorrow);
+        assert (credit.getCount() < count);
     }
 
 }

0765a18b9d1b5af66c42915ea94d0b3df3c5f1bf
Scott Rutherford <rutherford.scott@gmail.com>
2016-11-01 14:58:44 -0700

    added empty background classes

---
+++ app/src/main/java/com/example/scott/cs246finalproject/CalendarConnecgtor.java
@@ -0,0 +1,10 @@
+package com.example.scott.cs246finalproject;
+
+/**
+ * Created by scott on 11/1/16.
+ */
+
+public class CalendarConnecgtor {
+
+
+}
---
+++ app/src/main/java/com/example/scott/cs246finalproject/CalendarController.java
@@ -0,0 +1,15 @@
+package com.example.scott.cs246finalproject;
+
+/**
+ * Created by scott on 11/1/16.
+ */
+
+public class CalendarController {
+
+    private CalendarConnecgtor calendar;
+
+    private Credits credits;
+
+
+
+}

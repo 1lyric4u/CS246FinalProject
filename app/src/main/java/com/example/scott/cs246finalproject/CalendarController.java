2ffe9c0944a0077d3865a495ed302144e539b3b8
David Rosevear <davidjrosevear@gmail.com>
2016-11-02 21:05:00 -0600

    First tests - David

--- app/src/main/java/com/example/scott/cs246finalproject/CalendarController.java
+++ app/src/main/java/com/example/scott/cs246finalproject/CalendarController.java
@@ -1,5 +1,9 @@
 package com.example.scott.cs246finalproject;
 
+import android.view.View;
+
+import java.util.Date;
+
 /**
  * Created by scott on 11/1/16.
  */
@@ -10,6 +14,22 @@ public class CalendarController {
 
     private Credits credits;
 
+    // These numbers should probably be changed later.
+    public static final int GET_EVENTS = 1;
+    public static final int MOVE_EVENT = 2;
 
+    public void performAction(int action) {
+        // These could probably also be better implemented, just allowing for a test result
+        // If not one of the allowed actions, do nothing
+        if (action != GET_EVENTS && action != MOVE_EVENT) {
+            // Do nothing, but get out of the method
+            System.out.println("Please perform an acceptable action");
+            return;
+        }
 
+    }
+
+    public void update(View v) {
+        // Do something
+    }
 }

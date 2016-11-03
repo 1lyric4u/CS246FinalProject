bf9e721f91dba330d42161c55319bcfdac9ef6aa
Scott Rutherford <rutherford.scott@gmail.com>
2016-11-02 17:19:25 -0700

    modified use of Date objects to use non-decremented instantiation. Input values are unchanged from original.

--- app/src/main/java/com/example/scott/cs246finalproject/CalendarConnector.java
+++ app/src/main/java/com/example/scott/cs246finalproject/CalendarConnector.java
@@ -9,8 +9,10 @@ import java.util.List;
 
 public class CalendarConnector {
 
-
-    public boolean isValidViwer(){
+    // until this project has proper google-api imports available,
+    // Event references are replaced with Object references.
+    // This will change.
+    public boolean isValidViwer(String id){
         return false;
     }
 
@@ -18,5 +20,16 @@ public class CalendarConnector {
         return null;
     }
 
+    public void deleteEvent(Object e){
+
+    }
+
+    public void createEvent(Object e){
+
+    }
+
+    public boolean moveEvent(Object e){
+        return false;
+    }
 
 }
---
+++ app/src/test/java/com/example/scott/cs246finalproject/CalendarConnectorTest.java
@@ -0,0 +1,38 @@
+package com.example.scott.cs246finalproject;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by scott on 11/2/16.
+ */
+public class CalendarConnectorTest {
+
+    private final CalendarConnector connector = new CalendarConnector();
+
+    private static String TEST_E_MAIL = "rutherford.scott@gmail.com";
+
+    @Test
+    public void isValidViwer() throws Exception {
+        // a valid google e-mail account should return true
+        assertTrue(connector.isValidViwer(TEST_E_MAIL));
+    }
+
+    // instance should be an Event object but
+    // i need to find a way to get google-api imports
+    // to properly resolve first.
+    // Object type will change below, but otherwise the
+    // test is valid.
+    @Test
+    public void deleteEvent() throws Exception {
+        // change object type here
+        Object event = new Object();
+        // attempt deletion from google calendar
+        connector.deleteEvent(event);
+        // verify through call to calendar
+        assertFalse(connector.getCalendarEvents().contains(event));
+    }
+
+}

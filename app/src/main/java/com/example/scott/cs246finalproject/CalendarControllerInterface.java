4f7f45d9c6786ebd7983cecec26d6d0637d30e7c
David Rosevear <davidjrosevear@gmail.com>
2016-11-06 01:02:16 -0700

    Created some possible Interfaces for the View and Controller in MVC

---
+++ app/src/main/java/com/example/scott/cs246finalproject/CalendarControllerInterface.java
@@ -0,0 +1,17 @@
+package com.example.scott.cs246finalproject;
+
+/**
+ * Created by David Rosevear on 11/6/2016.
+ */
+
+public interface CalendarControllerInterface {
+
+    // Consider returning the appointments deleted, added, etc.
+    // Also, these probably need some sort of parameter!
+
+    public void createAppointment();
+    public void cancelAppointment();
+
+    // Other methods?
+
+}
---
+++ app/src/main/java/com/example/scott/cs246finalproject/MVC_View_Interface.java
@@ -0,0 +1,12 @@
+package com.example.scott.cs246finalproject;
+
+/**
+ * Created by David Rosevear on 11/6/2016.
+ */
+
+public interface MVC_View_Interface {
+
+    public void actionPerformed();
+
+    public void update();
+}

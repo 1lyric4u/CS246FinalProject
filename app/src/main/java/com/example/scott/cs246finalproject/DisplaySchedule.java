03422ef820d147c41b929dd4568ca83bed5b8934
Scott Rutherford <rutherford.scott@gmail.com>
2016-11-12 12:45:37 -0800

    Added 'fake' times to DisplaySchedule listView and provided Intent to send data to next activity.

--- app/src/main/java/com/example/scott/cs246finalproject/DisplaySchedule.java
+++ app/src/main/java/com/example/scott/cs246finalproject/DisplaySchedule.java
@@ -4,15 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+
+import static java.util.Calendar.HOUR_OF_DAY;
+import static java.util.Calendar.MINUTE;
+import static java.util.Calendar.SECOND;
 
 public class DisplaySchedule extends AppCompatActivity {
 
-
     public static final String APPOINTMENT = "Appointment";
+
+    private static final String EMPTY_VALUE = "no selection made";
+
+    private String listItemSelected;
+
+    private CalendarControllerInterface controller;
 
     private ListView listView;
 
@@ -24,25 +39,52 @@ public class DisplaySchedule extends AppCompatActivity {
         setContentView(R.layout.activity_display_schedule);
         // view elements which shall be modified by the user
         listView = (ListView) findViewById(R.id.list_of_events);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                listItemSelected = (String)listView.getItemAtPosition(i);
+            }// end override
+        });
         arrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item);
         listView.setAdapter(arrayAdapter);  // apply adapter to listView
 
-        // data comes from choooseSchedule
+        // grab controller
+
+
+        // populate time availability from controller
+        arrayAdapter.addAll(fakeTimes());
+
+        // default 'selected time' = false time
+        listItemSelected = EMPTY_VALUE;
     }
 
+    // execute controller method and send data onward
     private void selectTime(View view){
-        // execute controller method and update UI
+        // build and populate intent
+        Intent intent = new Intent(this, CancelConfirm.class);
+        intent.putExtra(APPOINTMENT, listItemSelected);
 
-        Intent intent = new Intent();
-        intent.putExtra(APPOINTMENT, "");
-
-        // data goes to cancelConfirm
-
+        // pass to CancelConfirm once selection is made
+        if(!listItemSelected.equals(EMPTY_VALUE))
+            startActivity(intent);
     }
 
-    private void updateListView(List<String> list){
-        arrayAdapter.clear();       // empty, in prep for new data
-        arrayAdapter.addAll(list);  // update UI
+    // to be removed and replaced with a call to the controller for available times
+    private List<String> fakeTimes(){
+        List<String> list = new ArrayList<>();
+        Calendar today = GregorianCalendar.getInstance();
+        today.set(HOUR_OF_DAY, 0);
+        today.set(MINUTE, 0);
+        today.set(SECOND, 0);
+
+        for(int count = 0; count < 24 * 4; count++){
+            Date time = today.getTime();
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss a");
+            list.add(sdf.format(time));
+            today.add(MINUTE, 15);
+        }// end loop
+
+        return list;
     }
 
 }

8f6ff0dbf1d80273e969266fca781e5a567d0276
shanna90 <sli15001@byui.edu>
2016-11-12 11:11:41 -0500

    Merge branch 'master' of https://github.com/1lyric4u/CS246FinalProject
    # Conflicts:
    #	app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java

--- app/src/main/java/com/example/scott/cs246finalproject/ChooseDay.java
+++ app/src/main/java/com/example/scott/cs246finalproject/ChooseDay.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 public class ChooseDay extends AppCompatActivity {
     /* this view needs to be accessed by the "reschedule" button on the home page*/
     /* the date chosen from the calendar view object needs to be sent to DisplaySchedule.java*/
+    /* use setMinDate() to start calendar tomorrow */
+    /*check date using creditCheck to verify there is a credit that will work for the selected day
+    if not, send the user to an alert page. Also, send to alert page if date selected is Sun/Sat
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

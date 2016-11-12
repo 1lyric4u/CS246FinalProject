11554d73ddc5092d2cdc3478345989eb9660ebf8
shanna90 <sli15001@byui.edu>
2016-11-12 09:17:47 -0500

    Merge branch 'master' of https://github.com/1lyric4u/CS246FinalProject
    # Conflicts:
    #	app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java

--- app/src/main/java/com/example/scott/cs246finalproject/CancelConfirm.java
+++ app/src/main/java/com/example/scott/cs246finalproject/CancelConfirm.java
@@ -4,52 +4,46 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
+
 
 public class CancelConfirm extends AppCompatActivity implements MVC_View_Interface {
-    /* Needs to get date to cancel from home page and display it in TextView ID:apptToCancel*/
 
-    /*Displays message saying "You will be given a credit on your homepage which will
-    allow you to reschedule" if the lesson time is more than 24 hrs away. If the cancel happens
-    withing 24 hrs of the appointment time, it should display "Please understand that because this
-    lesson is less than 24 hrs away, you will not be allowed to reschedule." This message should
-    be displayed in Text View ID:creditMsg
-     */
+    public Event eventToCancel;
 
     /* Button ID:buttonYes needs to alter schedule, add a credit, and return user to homepage*/
+    public void cancelAppt(View view) {
 
-    /*Button ID:buttonNo needs to simple return user to homepage*/
+    }
+
+    /*Button ID:buttonNo needs to simply return user to homepage*/
+    public void returnToMain() {
+        finish();
+    }
 
     // Gain access to the controller
     private CalendarController controller = CalendarController.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);Bundle extras = getIntent().getExtras();
         setContentView(R.layout.activity_cancel_confirm);
-    }
 
-    /**
-     * When an action is performed, pass the information on to the controller
-     * Note that is occurs in this activity when one of the two buttons are clicked
-     *
-     * @param view
+        /* Needs to get date to cancel from home page and display it in TextView ID:apptToCancel*/
+        TextView apptToCancel = (TextView) findViewById(R.id.apptToCancel);
+        if (extras != null) {
+            eventToCancel = extras.get(eventToSend); //eventToSend needs to come from main
+            apptToCancel.setText(eventToCancel.getStart().toString());
+        }
+
+        /*Displays message saying "You will be given a credit on your homepage which will
+    allow you to reschedule" if the lesson time is more than 24 hrs away. If the cancel happens
+    withing 24 hrs of the appointment time, it should display "Please understand that because this
+    lesson is less than 24 hrs away, you will not be allowed to reschedule." This message should
+    be displayed in Text View ID:creditMsg
      */
-    public void actionPerformed(View view) {
-        controller.actionPerformed(this, view);
-    }
-
-    public void update() {
-        // Do Something
-    }
-
-    /**
-     * Method to return to the MainActivity (or HomepageActivity) - consider making part of an
-     * interface.
-     */
-    public void returnToHomepage() {
-
-        // Start the MainActivity
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }

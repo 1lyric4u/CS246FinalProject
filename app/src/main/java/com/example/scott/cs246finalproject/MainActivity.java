f9a79eed9a47c644fd9783c93ab94d6b78a937a8
Shanna <sli15001@byui.edu>
2016-11-12 23:27:23 -0500

    Rejected commit: Default

--- app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java
+++ app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.scott.cs246finalproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 /**
@@ -18,11 +23,33 @@ public class MainActivity extends AppCompatActivity {
     * when clicked, needs to send the event as "eventToSend" to CancelConfirm as an extra.
     * CancelConfirm is already prepared to handle it*/
 
-    /* Needs to display number of credits available. This should be saved as a Shared Reference.
-    * The id is creditCount*/
+    //For some reason, the following line of code causes a crash, maybe because it is empty
+    //ListView mylist = (ListView) findViewById(R.id.upcoming);
+    //populate list
 
-    /*The button id:Resched needs to move to the view:ChooseDay. No information needs to be passed*/
+    // Gain access to the controller
+    //private CalendarController controller = CalendarController.getInstance();
 
+    /* Needs to display number of credits available. This should be saved in
+    * CalendarController.credits.getCount()
+    * The id for the text view is creditCount*/
+
+
+    /*The button id:Resched needs to move to ChooseDay. calendarcontroller needs to be passed */
+    public void toChooseDay (View view) {
+        Intent intent = new Intent(this, ChooseDay.class);
+        //pass controller, or at least credits somehow
+        startActivity(intent);
+    }
+
+    //this is commented out because it references mylist, which if initialized prevents app from loading
+    /*public void toCancelConfirm (View view) {
+        Intent intent = new Intent(this, CancelConfirm.class);
+        String eventToSend = mylist.getSelectedItem().toString();
+        intent.putExtra("cancel me", eventToSend);
+        //intent.putExtra("controller", controller) // not sure how to pass controller, sorry
+        startActivity(intent);
+    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

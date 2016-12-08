36d467ed120d83ddbf876b1adfebb3987a4f08ff
Shanna <sli15001@byui.edu>
2016-12-08 14:24:04 -0500

    Rejected commit: Default

--- app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java
+++ app/src/main/java/com/example/scott/cs246finalproject/MainActivity.java
@@ -31,16 +31,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView creditCount;
     public static String EVENTTOCANCEL;
 
-    /*The button id:Resched needs to move to ChooseDay.*/
+    //send user to Choose Day page
     public void toChooseDay(View view) {
         Intent intent = new Intent(this, ChooseDay.class);
         startActivity(intent);
-        // Scott - just testing logging with the method call below
-        //controller.createAppointment();
-        //controller.update(view);
-        //controller.cancelAppointment();
     }
 
+    //send user to Cancel Confirm page
     public void toCancelConfirm (View view) {
         Intent intent = new Intent(this, CancelConfirm.class);
         //because the listView is populated by CalendarConnector, we access that list.
@@ -55,38 +52,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //populate list
-        //This is now done by CalendarConnector
         controller.calendar.listView = (ListView) findViewById(R.id.upcoming);
         controller.calendar.adapter = new ArrayAdapter<>(
                 getApplicationContext(), R.layout.support_simple_spinner_dropdown_item);
-
-        //  !!! !!! !!!
-        // This will load all of the teacher's calendar, not just where student is an attendee
-        // Filtering code needs to go here !!!! <3 Shanna
-
-        /*
-        List<Event> upcoming = controller.calendar.getCalendarEvents();
-        listView = (ListView) findViewById(R.id.upcoming);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                listItemSelected = (String)listView.getItemAtPosition(i);
-            }// end override
-        });
-        arrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item);
-        listView.setAdapter(arrayAdapter);  // apply adapter to listView
-
-        arrayAdapter.addAll(upcoming.toString());
-        */
-
-        //display number of credits available.
-        //creditCount = (TextView) findViewById(R.id.creditCount);
-        //creditCount.setText(controller.credits.getCount());
-
-
-        // !!This should be replaced, but was used for testing purposes!!
-        //Intent intent = new Intent(this, CancelConfirm.class);
-        //startActivity(intent);
     }
+
+    //retrieves teacher's calendar info
     public void click(View view){controller.calendar.getResults(view);}
 }
